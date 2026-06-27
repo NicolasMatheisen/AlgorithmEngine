3905b1104388a61762987cee58411c61f5a93b5a
Nicolas Matheisen <nicolasmatheisen@icloud.com>
2026-06-28 00:59:11 +0200

    test

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,19 @@
-import js from "@eslint/js";
+import globals from 'globals';
 
 export default [
-  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node, 
+        ...globals.jest, 
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
+  },
 ];
