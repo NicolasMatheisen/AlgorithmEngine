d6e00d9bd9a4237ba3204e2e3b0013ba2c716e28
Nicolas Matheisen <nicolasmatheisen@icloud.com>
2026-06-29 00:30:07 +0200

    test

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,18 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser, 
+        ...globals.node,
+        ...globals.jest // <-- Das hier fügt die Jest-Globals (test, expect, etc.) hinzu!
+      } 
+    } 
+  },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
